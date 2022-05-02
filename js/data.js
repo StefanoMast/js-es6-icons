a346d6aecdf20cacd76c401e7b770740cbbf0187
StefanoMast <ste.mastrantonio@gmail.com>
2022-05-02 18:27:33 +0200

    esercizio base terminato

--- js/data.js
+++ js/data.js
@@ -25,17 +25,13 @@ const icons= [
 	{ name: 'user-secret',    prefix: 'fa-', type: 'user',      family: 'fas', color: 'blue'}
 ];
 
-// [Milestone 1
-// 	Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.
 
-//uso il ciclo forEach per creare il div in modo dinamico
 const container = document.getElementById("wrapper");
 
 icons.forEach(element => {
 	let div = document.createElement("div");
-	//Milestone 2
-// 	Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
-	divContent = div.innerHTML = `<div class="icon-box"> <div class="${element.type}"> <i class = "${element.family} ${element.prefix} ${element.name}"></i> </div> <h2>${element.name}</h2> </div>`
+
+	divContent = div.innerHTML = `<div class="icon-box"> <div class="${element.type}"> <i class = "${element.family} ${element.prefix}${element.name}"></i> </div> <h2>${element.name}</h2> </div>`
 	container.append(div); 
 });
 
